6422494787bbcfeefa0ce682b6fc89d5a2ee6839
River Jeong <ykjeong1223@naver.com>
2023-01-15 23:56:13 +0900

    bluetooth3

---
+++ test_23_bluetoothCommunicationSetting3_msg.ino
@@ -0,0 +1,29 @@
+#define bt_Tx 12
+#define bt_Rx 13
+
+#include <SoftwareSerial.h>
+SoftwareSerial BTSerial(bt_Tx, bt_Rx);
+char oneChar;
+String msg;
+
+void setup() {
+  Serial.begin(9600);
+  BTSerial.begin(9600);
+}
+void loop() {
+  if(Serial.available()){
+    BTSerial.write(Serial.read());
+  }  
+  //블루투스 통신으로 1byte씩 들어오는 것 누적하기
+  while(BTSerial.available()){
+   oneChar = BTSerial.read();
+   msg = msg + oneChar;
+   }
+   //Serial.println(msg);
+   
+   //메시지 누적한 String 타입 msg 출력
+   if(msg == "o"){
+    Serial.println("led on");
+   }
+   msg="";
+}
